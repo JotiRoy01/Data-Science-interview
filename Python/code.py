b5b359f2809f71cdd8b35c74a85a98298a0aa1f2
JotiRoy01 <jotiroygit@gmail.com>
2026-01-21 12:57:57 +0600

    Add initial Python script and documentation on variable identifiers

---
+++ Python/code.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+name = "joti" # write one instruction on one line
+age = 25
+PI = 3.1416
+
+print(name, age, PI)
---
+++ Python/python.md
@@ -0,0 +1,4 @@
+In python the variable is called the identifier
+bash```name = "joti"
+```
+here the identifier is name.
