e26ef3fcb440d62b5e6e0a93396a3874f00573e1
JotiRoy01 <jotiroygit@gmail.com>
2026-01-21 19:33:59 +0600

    complete first lecture

--- Python/code.py
+++ Python/code.py
@@ -1,7 +1,40 @@
-print("hello world")
+# print("hello world")
 
-name = "joti" # write one instruction on one line
-age = 25
-PI = 3.1416
+# name = "joti" # write one instruction on one line
+# age = 25
+# PI = 3.1416
 
-print(name, age, PI)
+# num = 2
+# isPrime = True
+# print(name, age, PI)
+# print(type(age))
+
+
+a = 5
+b = 10
+
+sum = a + b
+print(sum)
+
+#Operator
+a = 10
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#relation
+var = False
+print(not var) # Now the var is true
+print(not ( 5 > 8)) #true
+#not operator reverse the statement
+
+x = 3
+x += 5
+print(x)
+
+# Type Conversion
+ans1 = int(5 + 10.7) # type casting
+ans2 = 5 + 10.7 # type conversion
--- Python/python.md
+++ Python/python.md
@@ -3,3 +3,26 @@ In python the variable is called the identifier
 name = "joti"
 ```
 here the identifier is name.
+all identifier allow the 
+* . A-Z, a-z
+* . digit 0-9 //Don't start the identifier with digit
+*  _
+
+### Style Guide
+- total_price = 100 -> snake_case <br> 
+- totalPrice = 100 -> camelCase  
+- TotalPrice = 100 -> PascalCase
+
+### Operator
+print(a**b) -> Calculate the power
+
+>> And operator
+
+| Result 1 | v1 | v2  |
+|----------|----------|----------|
+| T   | T   | T  |
+| F   | T   | F  |
+|F|F|T|
+|F|F|F|
+
+
